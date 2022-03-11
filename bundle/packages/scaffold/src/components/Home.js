98d708b207caf4e55d16d7f18246a3b2cd4a91d7
Nick Allen <nallen@esolutionsone.com>
2022-03-11 14:48:46 -0600

    fetch working from custom package

--- bundle/packages/scaffold/src/components/Home.js
+++ bundle/packages/scaffold/src/components/Home.js
@@ -1,27 +1,45 @@
-import React from "react";
-import { compose, lifecycle } from "recompose";
-import { connect } from "../redux/store";
+import React, { useEffect } from 'react';
+import { compose, lifecycle } from 'recompose';
+import { connect } from '../redux/store';
+import { fetchKapp } from '@kineticdata/react';
 
-const HomeComponent = (props) => (
-  <div className="page-container">
-    <div className="page-panel">
-      <div className="p-5 text-center">
-        <h1>Welcome to the Scaffold Package!</h1>
-        <p>This is the home page.</p>
+const HomeComponent = props => {
+  console.log('scaffold loaded');
+
+  useEffect(() => {
+    console.log('scaffold mounted');
+    const getKapp = async () => {
+      let response = await fetchKapp({ kappSlug: 'nick-sandbox' });
+      console.log(response);
+    };
+
+    getKapp();
+  }, []);
+
+  return (
+    <div className="page-container">
+      <div className="page-panel">
+        <div className="p-5 text-center">
+          <h1>Welcome to the Scaffold Package!</h1>
+          <p>This is the home page.</p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   appLocation: state.app.location,
 });
 
 const mapDispatchToProps = {};
 
 export const Home = compose(
-  connect(mapStateToProps, mapDispatchToProps),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
   lifecycle({
     componentDidMount() {},
-  })
+  }),
 )(HomeComponent);
