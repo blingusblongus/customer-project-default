da7ae0a8d7cbd47e86bde8b0887d15e6455b59fd
Nick Allen <nallen@esolutionsone.com>
2022-03-14 10:30:08 -0500

    add mapped attributes

--- bundle/packages/scaffold/src/components/KappAccordion/KappAccordion.jsx
+++ bundle/packages/scaffold/src/components/KappAccordion/KappAccordion.jsx
@@ -21,60 +21,24 @@ export default function KappAccordion({ kapps }) {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                <ol>
-                  {kapp.attributes?.map((attr, i) => {
-                    return (
-                      <li key={i}>
-                        {attr.name}: {attr.values[0]}
-                      </li>
-                    );
-                  })}
-                </ol>
+                {kapp.attributes.length > 0 ? (
+                  <ol>
+                    {kapp.attributes?.map((attr, i) => {
+                      return (
+                        <li key={i}>
+                          {attr.name}: {attr.values[0]}
+                        </li>
+                      );
+                    })}
+                  </ol>
+                ) : (
+                  <div>No Attributes</div>
+                )}
               </Typography>
             </AccordionDetails>
           </Accordion>
         );
       })}
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
-      </Accordion>
     </div>
   );
 }
